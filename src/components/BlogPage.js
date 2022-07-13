ac6054f3f347acf595e17063475315b5c861514b
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-07-13 11:53:51 +0100

    main

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -95,7 +95,7 @@ const BlogPage = () => {
 </Grid>
 
 </Center>
-<BigTitle text="BLOG" top="5rem" left="5rem" />
+<BigTitle text="RESUME" top="5rem" left="5rem" />
             </Container>
         </MainContainer>
     )

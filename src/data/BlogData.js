01ab8be8c99195fabf2b5b49aaae008c78dcbd69
Promise Emmanuel <95595405+Promise-csharp@users.noreply.github.com>
2022-10-28 15:51:53 +0100

    Update BlogData.js

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -7,7 +7,7 @@ export const Blogs = [
         tags:[""],
         date:"",
         imgSrc:"https://i.ibb.co/KD9D2My/resume.png",
-        link:"https://github.com/Diapmusic/Diap/raw/main/src/assets/PromiseCV1.pdf"
+        link:"https://github.com/Promise-csharp/Diap/raw/main/src/assets/Promise-Resume.pdf"
   
       },
     
